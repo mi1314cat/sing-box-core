a4ba007b334c1ff94e2cfc545a62ab0536d8a0cd
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2025-05-05 16:24:57 +0800

    Update singbox.sh

--- singbox.sh
+++ singbox.sh
@@ -431,11 +431,7 @@ public_key=$(echo "$key_pair" | awk '/PublicKey/ {print $2}' | tr -d '"')
 short_id=$(/root/catmi/singbox/sing-box generate rand --hex 8)
 hy_password=$(/root/catmi/singbox/sing-box generate rand --hex 8)
 
-# 获取公网 IP 地址
-PUBLIC_IP_V4=$(curl -s https://api.ipify.org)
-PUBLIC_IP_V6=$(curl -s https://api64.ipify.org)
-echo "公网 IPv4 地址: $PUBLIC_IP_V4"
-echo "公网 IPv6 地址: $PUBLIC_IP_V6"
+
 
 # 获取公网 IP 地址
 PUBLIC_IP_V4=$(curl -s https://api.ipify.org)
